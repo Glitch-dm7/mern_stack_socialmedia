91a01b9bab44f151d98a912e6cf56b520a42b536
Glitch-dm7 <divyadarshandvd007@gmail.com>
2023-02-09 17:47:59 +0530

    Deployement Changes

--- client2/src/api/index.js
+++ client2/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: 'https://glitch-anime-club.vercel.app' })
+const API = axios.create({ baseURL: 'https://crazy-plum-pantsuit.cyclic.app' })
 
 API.interceptors.request.use((req) => {
     if(localStorage.getItem('profile')) {
